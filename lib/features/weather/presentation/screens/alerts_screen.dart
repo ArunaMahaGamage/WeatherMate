05f5998bc86abd74a4754d055bb527a26cb2ff36
Aruna <admgbit@gmail.com>
2025-12-08 22:46:51 +0530

    Added alert

--- lib/features/weather/presentation/screens/alerts_screen.dart
+++ lib/features/weather/presentation/screens/alerts_screen.dart
@@ -20,18 +20,29 @@ class _AlertsScreenState extends State<AlertsScreen> {
             icon: const Icon(Icons.arrow_back),
             onPressed: () => context.go('/'),
           ),
-          title: const Text('Alerts & Filters')),
+          title: const Text('Alerts')),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
+        child: ListView(
           children: [
-            SwitchListTile(title: const Text('Extreme heat alerts'), value: extremeHeat, onChanged: (v) => setState(() => extremeHeat = v)),
-            SwitchListTile(title: const Text('Heavy rain alerts'), value: heavyRain, onChanged: (v) => setState(() => heavyRain = v)),
-            SwitchListTile(title: const Text('High wind alerts'), value: highWind, onChanged: (v) => setState(() => highWind = v)),
+            Text('Hour Rainfall Map'),
             const SizedBox(height: 16),
-            ElevatedButton(onPressed: () {}, child: const Text('Save preferences')),
-            const SizedBox(height: 8),
-            const Text('Note: You can extend this to real notifications or filtered city lists.'),
+            Image.network(
+              'https://meteo.gov.lk/images/SLMap.jpg?v=1765213460',
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return Center(child: CircularProgressIndicator());
+              },
+              errorBuilder: (context, error, stackTrace) {
+                return Icon(Icons.error);
+              },
+            ),
+            //SwitchListTile(title: const Text('Extreme heat alerts'), value: extremeHeat, onChanged: (v) => setState(() => extremeHeat = v)),
+            //SwitchListTile(title: const Text('Heavy rain alerts'), value: heavyRain, onChanged: (v) => setState(() => heavyRain = v)),
+            //SwitchListTile(title: const Text('High wind alerts'), value: highWind, onChanged: (v) => setState(() => highWind = v)),
+            const SizedBox(height: 16),
+            //ElevatedButton(onPressed: () {}, child: const Text('Save preferences')),
+            //const SizedBox(height: 8),
           ],
         ),
       ),
